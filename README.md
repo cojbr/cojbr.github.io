4a6ae9b4991885c969b57525fe14d2f6134d8a6e
Cody Breen <codybreen@Codys-MacBook-Air.local>
2019-12-08 17:48:16 -0500

    Initial commit to github pages.

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# cojbr.github.io
+# Blackjack! v1.0
+
+Welcome to my Blackjack game! This is my first stab at a true card game. It's not perfect but I've learned a lot from building this. 
+
+## Technologies Used
+
+This is solely compiled using JavaScript, CSS and HTML. I tried to create this without using jQuery, to hone my JavaScript skills.
+
+
+### Let's Play!
+
+It's a very quick and simple playthrough, but some of the best things in life are the simple things. I hope you enjoy!
+
+
+There are three steps to play:
+
+- First, press the START button on the top left of the screen.
+
+<img src="https://i.imgur.com/O5iREqJ.png"
+     alt="cojbr's Blackjack Start Button"
+     style="float: left; margin-right: 10px;" />
+
+
+- Next, view your scores at the bottom of the screen to determine your next move.
+
+<img src="https://i.imgur.com/bpcVDUQ.png"
+     alt="cojbr's Blackjack Start Button"
+     style="float: left; margin-right: 10px;" />
+
+
+- Use HIT or STAND to play through the hands until a winner is decided. Your cards will be displayed as their text values (Clubs K, Diamonds 7, etc.). 
+
+<img src="https://i.imgur.com/fgZmzZ3.png"
+     alt="cojbr's Blackjack Hit or Stand"
+     style="float: left; margin-right: 10px;" />
+
+
+You will be prompted on whether you or the dealer has won. 
+
+<img src="https://i.imgur.com/BIoOdzz.png"
+     alt="cojbr's Blackjack Winner Dialog"
+     style="float: left; margin-right: 10px;" />
+
+<img src="https://i.imgur.com/dgoJwRe.png"
+     alt="cojbr's Blackjack Winner Dialog"
+     style="float: left; margin-right: 10px;" />
+
+<img src="https://i.imgur.com/77FYD2m.png"
+     alt="cojbr's Blackjack Winner Dialog"
+     style="float: left; margin-right: 10px;" />
+
+<img src="https://i.imgur.com/8UI3PeO.png"
+     alt="cojbr's Blackjack Winner Dialog"
+     style="float: left; margin-right: 10px;" />
+
+CLICK THE LINK BELOW TO PLAY:
+
+#### Next Steps 
+
+Updates to the game coming in v1.1, scheduled to release later this year. The release of v1.0 was to build a working game before adding further functionality. Planned functionality for v1.1 includes:
+
+- Functional betting tool. 
+
+- "7 of Spades", "K of Hearts" etc displaying. Possibly images of cards instead. 
+
+- Dealer will stand after placing score of 17.
+
+
+#### That's all, folks!
+
+Thank you for playing, reading, and please feel free to fork the game and push me any improvements you think would make this game awesome!!
+
+Best,
+Cody Breen
---
+++ js/main.js
@@ -0,0 +1,157 @@
+class Game {
+    constructor() {
+        this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+        this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+        this.playersHand = []
+        this.dealersHand = []
+        this.deck = []
+        this.startButton = document.getElementById('start-button')
+        this.hitButton = document.getElementById('button-hit')
+        this.hitButton.disabled = true
+        this.standButton = document.getElementById('button-stand')
+        this.standButton.disabled = true
+        this.playerScore = document.getElementById('players-score')
+        this.dealerScore = document.getElementById('dealers-score')
+        this.playersCardsInPlay = document.getElementById('players-hand')
+        this.dealersCardsInPlay = document.getElementById('dealers-hand')
+        this.gameStatus = document.getElementById('game-status')
+        this.resetButton = document.getElementById('reset-button')
+        this.addEvents()
+    }
+
+    addEvents() {
+        this.startButton.addEventListener('click', () => this.startGame())
+        this.hitButton.addEventListener('click', () => this.handleHit())
+        this.standButton.addEventListener('click', () => this.handleStand())
+        this.resetButton.addEventListener('click', () => this.reset())
+    }
+
+    startGame() {
+        this.deck = this.generateDeck()
+        this.playersHand = [this.getCard(), this.getCard()]
+        this.dealersHand = [this.getCard(), this.getCard()]
+        this.hitButton.disabled = false
+        this.standButton.disabled = false
+        this.startButton.disabled = true
+        this.render()
+    }
+
+    handleHit() {
+        this.playersHand.push(this.getCard())
+        this.dealersHand.push(this.getCard()) 
+        this.render()
+        this.checkForWinner()
+    }
+
+    handleStand() {
+        this.dealersHand.push(this.getCard())
+        this.hitButton.style.display = 'none';
+        this.render()
+        this.checkForWinner()
+    }
+
+    generateDeck() {
+        let deckArray = [];
+        for (let s = 0; s < this.suits.length; s++) {
+            for (let v = 0; v < this.values.length; v++) {
+                let newCard = {
+                    suit: this.suits[s],
+                    value: this.values[v],
+                };
+                deckArray.push(newCard);
+            }
+        }
+        return deckArray;
+    }
+
+    getCard() {
+        let randomIndex = Math.floor(Math.random() * this.deck.length)
+        let card = this.deck[randomIndex]
+        this.deck.splice(randomIndex, 1)
+        return card
+    }
+
+    calculateScore(userHand) {
+        let sum = 0
+        let aceCount = 0
+        for (let i = 0; i < userHand.length; i++) {
+            let value = userHand[i].value
+            if (value === 'J' || value === 'Q' || value === 'K') {
+                sum += 10;
+            } else if (value === 'A') {
+                sum += 11
+                aceCount++
+            } else {
+                sum += value
+            }
+        }   
+
+        while (sum > 21 && aceCount > 0) {
+            sum -= 10
+            aceCount -= 1
+        }
+        return sum
+    }
+
+    checkForWinner() {
+        let playerScore = this.calculateScore(this.playersHand)
+        let dealerScore = this.calculateScore(this.dealersHand)
+
+        if (playerScore > 21) {
+            this.gameStatus.innerHTML = `<p>"Game Over. You busted. Dealer wins."</p>`
+            this.hitButton.disabled = true
+            this.standButton.disabled = true
+            this.startButton.disabled = false
+
+        } else if (dealerScore > 21) {
+            this.gameStatus.innerHTML = `<p>"Game Over. Dealer busted. YOU WIN!"</p>`
+            this.hitButton.disabled = true
+            this.standButton.disabled = true
+            this.startButton.disabled = false
+
+        } else if (playerScore === 21) {
+            this.gameStatus.innerHTML = `<p>"Blackjack. YOU WIN!"</p>`
+            this.hitButton.disabled = true
+            this.standButton.disabled = true
+            this.startButton.disabled = false
+            
+        } else if (dealerScore === 21) {
+            this.gameStatus.innerHTML = `<p>"Blackjack. Dealer wins!"</p>`
+            this.hitButton.disabled = true
+            this.standButton.disabled = true
+            this.startButton.disabled = false
+        }
+    }   
+
+    displayCards() {
+        this.playersCardsInPlay.innerHTML = ''
+        for (let i = 0; i < this.playersHand.length; i++) {
+            this.playersCardsInPlay.innerHTML += JSON.stringify(this.playersHand[i]).replace(/{"suit":"/g,'').replace(/","/g,' ').replace(/value":/g,'').replace(/}/g,'').replace(/"/g,'') + "</br>"
+        }
+
+        this.dealersCardsInPlay.innerHTML = ''
+        for (let i = 0; i < this.dealersHand.length; i++) {
+            this.dealersCardsInPlay.innerHTML += JSON.stringify(this.dealersHand[i]).replace(/{"suit":"/g,'').replace(/","/g,' ').replace(/value":/g,'').replace(/}/g,'').replace(/"/g,'') + "</br>"
+        }
+    }
+
+    render() {
+        this.playerScore.innerHTML = `<p>Player's Score: ${this.calculateScore(this.playersHand)}</p>`
+        this.dealerScore.innerHTML = `<p>Dealer's Score: ${this.calculateScore(this.dealersHand)}</p>`
+        this.hitButton.style.display = ''
+        this.displayCards();
+    }
+
+    reset() {
+        this.startGame()
+        this.gameStatus.innerHTML = '<p></p>'
+    }
+}
+
+const game = new Game()
+
+
+
+
+
+
